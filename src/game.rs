d4e01305366f147bc25db91b2a811f222c89d3cf
Alexander Jophus <alextrelore@gmail.com>
2024-09-14 13:17:46 +0100

    remove pause button, control camera and placeholder separately

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,3 @@
-use std::time::Duration;
-
 use super::GameState;
 use bevy::color::palettes::css::WHITE;
 use bevy::gltf::Gltf;
@@ -8,26 +6,19 @@ use bevy_asset_loader::prelude::*;
 use bevy_rapier3d::prelude::*;
 use leafwing_input_manager::prelude::*;
 
-#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
-enum GameplayState {
-    #[default]
-    Playing,
-    Paused,
-}
-
 #[derive(PartialEq, Eq, Clone, Copy, Hash, Debug, Reflect)]
 enum PlayerAction {
-    Pause,
-    Move,
-    Shoot,
+    MoveCamera,
+    MovePlaceholderTower,
+    Place,
 }
 
 impl Actionlike for PlayerAction {
     fn input_control_kind(&self) -> InputControlKind {
         match self {
-            PlayerAction::Pause => InputControlKind::Button,
-            PlayerAction::Move => InputControlKind::DualAxis,
-            PlayerAction::Shoot => InputControlKind::Button,
+            PlayerAction::MoveCamera => InputControlKind::DualAxis,
+            PlayerAction::MovePlaceholderTower => InputControlKind::DualAxis,
+            PlayerAction::Place => InputControlKind::Button,
         }
     }
 }
@@ -43,15 +34,17 @@ pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((InputManagerPlugin::<PlayerAction>::default(),))
-            .init_state::<GameplayState>()
             .init_resource::<ActionState<PlayerAction>>()
             .insert_resource(PlayerAction::default_input_map())
             .add_systems(OnEnter(GameState::Game), setup)
-            .add_systems(Update, toggle_pause.run_if(in_state(GameState::Game)))
             .add_systems(Update, control_camera.run_if(in_state(GameState::Game)))
-            .add_systems(Update, shoot.run_if(in_state(GameState::Game)))
-            .add_systems(Update, move_bullets.run_if(in_state(GameState::Game)))
-            .add_systems(Update, despawn_shots.run_if(in_state(GameState::Game)));
+            .add_systems(
+                Update,
+                control_placeholder.run_if(in_state(GameState::Game)),
+            )
+            .add_systems(Update, place_tower.run_if(in_state(GameState::Game)));
+        // .add_systems(Update, move_bullets.run_if(in_state(GameState::Game)))
+        // .add_systems(Update, despawn_shots.run_if(in_state(GameState::Game)));
     }
 }
 
@@ -61,14 +54,14 @@ impl PlayerAction {
         let mut input_map = InputMap::default();
 
         // Default gamepad input bindings
-        input_map.insert_dual_axis(Self::Move, GamepadStick::LEFT);
-        input_map.insert(Self::Pause, GamepadButtonType::Start);
-        input_map.insert(Self::Shoot, GamepadButtonType::South);
+        input_map.insert_dual_axis(Self::MoveCamera, GamepadStick::LEFT);
+        input_map.insert_dual_axis(Self::MovePlaceholderTower, GamepadStick::RIGHT);
+        input_map.insert(Self::Place, GamepadButtonType::South);
 
         // Default kbm input bindings
-        input_map.insert_dual_axis(Self::Move, KeyboardVirtualDPad::WASD);
-        input_map.insert(Self::Pause, KeyCode::Escape);
-        input_map.insert(Self::Shoot, KeyCode::Space);
+        input_map.insert_dual_axis(Self::MoveCamera, KeyboardVirtualDPad::WASD);
+        input_map.insert_dual_axis(Self::MovePlaceholderTower, KeyboardVirtualDPad::ARROW_KEYS);
+        input_map.insert(Self::Place, KeyCode::Space);
 
         input_map
     }
@@ -77,6 +70,10 @@ impl PlayerAction {
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
 struct Camera;
+
+#[derive(Reflect, Component, Default)]
+#[reflect(Component)]
+struct TowerPlaceholder;
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
@@ -104,7 +101,7 @@ fn setup(mut commands: Commands, assets: Res<GltfAssets>, assets_gltf: Res<Asset
                     shape: Some(ComputedColliderShape::TriMesh),
                     ..Default::default()
                 },
-                Tower,
+                TowerPlaceholder,
             ))
             .with_children(|parent| {
                 parent.spawn(SpotLightBundle {
@@ -129,33 +126,30 @@ fn setup(mut commands: Commands, assets: Res<GltfAssets>, assets_gltf: Res<Asset
     }
 }
 
-fn toggle_pause(
-    state: Res<State<GameplayState>>,
-    action_state: Res<ActionState<PlayerAction>>,
-    mut next_state: ResMut<NextState<GameplayState>>,
-) {
-    if action_state.just_pressed(&PlayerAction::Pause) {
-        match state.get() {
-            GameplayState::Playing => next_state.set(GameplayState::Paused),
-            GameplayState::Paused => next_state.set(GameplayState::Playing),
-        }
-    }
-}
-
 fn control_camera(
     time: Res<Time>,
     action_state: Res<ActionState<PlayerAction>>,
     mut query: Query<&mut Transform, With<Camera>>,
 ) {
     let mut player_transform = query.single_mut();
-    let move_delta =
-        time.delta_seconds() * action_state.clamped_axis_pair(&PlayerAction::Move).xy();
-    player_transform.translation += Vec3::new(move_delta.x, 0.0, move_delta.y);
+    let move_delta = time.delta_seconds()
+        * action_state
+            .clamped_axis_pair(&PlayerAction::MoveCamera)
+            .xy();
+    player_transform.translation += Vec3::new(move_delta.x, 0.0, -move_delta.y);
+}
 
-    // impl pause
-    if action_state.just_pressed(&PlayerAction::Pause) {
-        println!("Paused!")
-    }
+fn control_placeholder(
+    time: Res<Time>,
+    action_state: Res<ActionState<PlayerAction>>,
+    mut query: Query<&mut Transform, With<TowerPlaceholder>>,
+) {
+    let mut player_transform = query.single_mut();
+    let move_delta = time.delta_seconds()
+        * action_state
+            .clamped_axis_pair(&PlayerAction::MovePlaceholderTower)
+            .xy();
+    player_transform.translation += Vec3::new(move_delta.x, 0.0, -move_delta.y);
 }
 
 #[derive(Reflect, Component, Default)]
@@ -171,47 +165,28 @@ struct ShotTimer {
     timer: Timer,
 }
 
-fn shoot(
+fn place_tower(
     action_state: Res<ActionState<PlayerAction>>,
-    mut meshes: ResMut<Assets<Mesh>>,
     mut commands: Commands,
-    query: Query<Entity, With<Tower>>,
+    placeholder_query: Query<&Transform, With<TowerPlaceholder>>,
+    assets: Res<GltfAssets>,
+    assets_gltf: Res<Assets<Gltf>>,
 ) {
-    if action_state.just_pressed(&PlayerAction::Shoot) {
-        let parent = query.single();
-        commands.entity(parent).with_children(|p| {
-            p.spawn((
-                ShotTimer {
-                    timer: Timer::new(Duration::from_secs(2), TimerMode::Once),
+    let placeholder = placeholder_query.single();
+    if action_state.just_pressed(&PlayerAction::Place) {
+        if let Some(gltf) = assets_gltf.get(&assets.charmander.clone()) {
+            commands.spawn((
+                SceneBundle {
+                    scene: gltf.scenes[0].clone(),
+                    transform: placeholder.looking_to(Vec3::Z, Vec3::Y),
+                    ..Default::default()
                 },
-                Bullet {
-                    direction: Vec3::new(0.0, 0.0, 1.0),
-                    speed: 5.0,
+                AsyncSceneCollider {
+                    shape: Some(ComputedColliderShape::TriMesh),
+                    ..Default::default()
                 },
-                PbrBundle {
-                    mesh: meshes.add(Sphere::new(0.05)),
-                    transform: Transform::from_translation(Vec3::new(0.0, 0.45, 0.0)),
-                    ..default()
-                },
+                Tower,
             ));
-        });
-    }
-}
-
-fn move_bullets(mut bullets: Query<(&Bullet, &mut Transform)>, time: Res<Time>) {
-    for (bullet, mut transform) in &mut bullets {
-        transform.translation += bullet.direction.normalize() * bullet.speed * time.delta_seconds();
-    }
-}
-
-fn despawn_shots(mut commands: Commands, mut q: Query<(Entity, &mut ShotTimer)>, time: Res<Time>) {
-    for (entity, mut shot_timer) in q.iter_mut() {
-        // timers gotta be ticked, to work
-        shot_timer.timer.tick(time.delta());
-
-        // if it finished, despawn the shot
-        if shot_timer.timer.finished() {
-            commands.entity(entity).despawn();
         }
     }
 }
