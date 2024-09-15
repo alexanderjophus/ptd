781046fdf22b7162d894b1c0d8083020106015c5
Alexander Jophus <alextrelore@gmail.com>
2024-09-15 11:04:14 +0100

    add grid system

--- src/debug.rs
+++ src/debug.rs
@@ -1,5 +1,8 @@
 use bevy::{input::common_conditions::input_toggle_active, prelude::*};
+use bevy_infinite_grid::{InfiniteGridBundle, InfiniteGridPlugin};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+use crate::GameState;
 
 pub struct DebugPlugin;
 
@@ -7,6 +10,15 @@ impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(
             WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Escape)),
+        )
+        .add_plugins(InfiniteGridPlugin)
+        .add_systems(
+            OnEnter(GameState::Game),
+            debug_system.run_if(input_toggle_active(true, KeyCode::F1)),
         );
     }
+}
+
+fn debug_system(mut commands: Commands) {
+    commands.spawn(InfiniteGridBundle::default());
 }
